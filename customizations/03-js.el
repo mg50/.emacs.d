cb4499d61577be2a4cf98a707b48a5c0c43e1bfd
Mark Grant <mgrant@mgrant-l.corp.instructure.com>
2017-01-12 17:39:45 -0600

    use js-jsx-mode

---
+++ customizations/03-js.el
@@ -0,0 +1,2 @@
+(add-to-list 'auto-mode-alist '("\\.js?\\'" . js-jsx-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js-jsx-mode))
--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 (require 'pallet)
@@ -12,6 +19,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(coffee-tab-width 2)
+ '(css-indent-offset 2)
  '(cua-enable-cua-keys nil)
  '(custom-safe-themes
    (quote
